a4a6c68cddabc66c99afd4c0e0a2328d9e0ab616
DongOk <hcan159@naver.com>
2024-06-16 02:46:49 +0900

    수업시간 과제 추가
    iris, 포켓몬

---
+++ 과제/pokemon_train.py
@@ -0,0 +1,32 @@
+# Pokemon
+
+### Step 1. 필요한 라이브러리 들을 import 하세요
+
+import pandas as pd
+
+### Step 2. 다음과 같은 dictionary를 생성하세요
+
+raw_data = {"name": ['Bulbasaur', 'Charmander','Squirtle','Caterpie'],
+            "evolution": ['Ivysaur','Charmeleon','Wartortle','Metapod'],
+            "type": ['grass', 'fire', 'water', 'bug'],
+            "hp": [45, 39, 44, 45],
+            "pokedex": ['yes', 'no','yes','no']                        
+            }
+
+### Step 3. pokemon이라는 변수에 DataFrame 을 할당하세요
+
+
+
+
+### Step 4. DataFrame 열이 알파벳 순서로 되어있습니다. name, type, hp, evolution, pokedex 의 순서로 column을 배치하세요
+
+
+
+
+### Step 5. "place" column을 만들고 ['park','street','lake','forest'] 순서로 할당하세요
+
+
+
+
+### Step 6. 각 column의 datatype을 확인해보세요
+
